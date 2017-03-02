c03d37f00d84faaf97e9f83fc78544ded316cbd9
AonaSuzutsuki <rvvfake.org@gmail.com>
2017-03-02 18:55:42 +0900

    検索画面の修正

--- SyousetsukaGetter/ViewModel/MainWindowViewModel.cs
+++ SyousetsukaGetter/ViewModel/MainWindowViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel.Syndication;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Xml;
 
 namespace SyousetsukaGetter.ViewModel
 {
