1c78576a24d11a8c4f2191aeaa49dd0a4b1a95e2
池上拓 <takuron1996@gmail.com>
2017-03-03 23:02:29 +0900

    Xml (#21)
    * Main.csを変更
    
    * パッケージを追加
    
    * 途中経過
    
    * no message
    
    * Revert "no message"
    
    This reverts commit 28fed85474bb781473f5a1d87d57528bf8b3ad24.
    
    * Fix Conflict

--- SyousetukaGetterLib/PageDownloader.cs
+++ SyousetukaGetterLib/PageDownloader.cs
@@ -1,25 +1,64 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace SyousetukaGetterLib
 {
     class PageDownloader
     {
-        private NovelUrlManager url = new NovelUrlManager();
+        private NovelUrlManager novelUrl = new NovelUrlManager();
 
+        /*
+         * 小説のダウンロードを行う。
+         * 
+         * */
         public void StartDownload(int index)
         {
-            string[] urls = url.Novel(index);
-
+            string[] urls = novelUrl.Novel(index);
+            foreach (string url in urls)
+            {
+                string html = GetHtml(url);
+                AnalysisHtml(url);
+            }
+            return;
+        }
+        /*
+         * 指定した小説のページから本文をHtmlで取得する。
+         * 
+         * */
+        private string GetHtml(string url)
+        {
+            var wc = new WebClient();
+            var st = wc.OpenRead(url);
+            var enc = Encoding.GetEncoding("UTF-8");
+            var sr = new StreamReader(st, enc);
+            string html = sr.ReadToEnd();
+            sr.Close();
+            st.Close();
+            return html;
+        }
+        /*
+         * Htmlのデータを解析する。
+         * 
+         * */
+        private void AnalysisHtml(string html)
+        {
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.OptionAutoCloseOnEnd = false;  //最後に自動で閉じる（？）
+            doc.OptionCheckSyntax = false;     //文法チェック。
+            doc.OptionFixNestedTags = true;    //閉じタグが欠如している場合の処理
+            doc.LoadHtml(html);
+            HtmlAgilityPack.HtmlNode node = doc.DocumentNode.SelectSingleNode("div[@id='novel_honbun']");
+            System.Console.WriteLine(node.InnerHtml);
+            System.Console.ReadLine();
             return;
         }
 
-
+        
 
 
 
