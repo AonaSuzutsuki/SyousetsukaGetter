44b52b5760e13df57f005f0806ac16be01045dbd
AonaSuzutsuki <rvvfake.org@gmail.com>
2017-03-05 13:29:01 +0900

    小説削除機能の追加

---
+++ SyousetsukaGetter/ExMessageBox/ExMessageBox.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace SyousetsukaGetter.ExMessageBox
+{
+    public static class ExMessageBoxBase
+    {
+        [DllImport("user32.dll")]
+        static extern Int32 FlashWindowEx(ref FLASHWINFO pwfi);
+        [StructLayout(LayoutKind.Sequential)]
+        private struct FLASHWINFO
+        {
+            public UInt32 cbSize;    // FLASHWINFO構造体のサイズ
+            public IntPtr hwnd;      // 点滅対象のウィンドウ・ハンドル
+            public UInt32 dwFlags;   // 以下の「FLASHW_XXX」のいずれか
+            public UInt32 uCount;    // 点滅する回数
+            public UInt32 dwTimeout; // 点滅する間隔（ミリ秒単位）
+        }
+        // 点滅を止める
+        private const UInt32 FLASHW_STOP = 0;
+        // タイトルバーを点滅させる
+        private const UInt32 FLASHW_CAPTION = 1;
+        // タスクバー・ボタンを点滅させる
+        private const UInt32 FLASHW_TRAY = 2;
+        // タスクバー・ボタンとタイトルバーを点滅させる
+        private const UInt32 FLASHW_ALL = 3;
+        // FLASHW_STOPが指定されるまでずっと点滅させる
+        private const UInt32 FLASHW_TIMER = 4;
+        // ウィンドウが最前面に来るまでずっと点滅させる
+        private const UInt32 FLASHW_TIMERNOFG = 12;
+        public static void FlashWind(IntPtr handle)
+        {
+            FLASHWINFO fInfo = new FLASHWINFO();
+            fInfo.cbSize = Convert.ToUInt32(Marshal.SizeOf(fInfo));
+            fInfo.hwnd = handle;
+            fInfo.dwFlags = FLASHW_ALL;
+            fInfo.uCount = 5;         // 点滅する回数
+            fInfo.dwTimeout = 0;
+
+            FlashWindowEx(ref fInfo);
+        }
+
+
+
+        public enum MessageType : int
+        {
+            /// <summary>
+            /// メッセージ（情報）
+            /// </summary>
+            Asterisk = 0,
+            /// <summary>
+            /// 一般の警告音
+            /// </summary>
+            Beep = 1,
+            /// <summary>
+            /// メッセージ（警告）
+            /// </summary>
+            Exclamation = 2,
+            /// <summary>
+            /// システムエラー
+            /// </summary>
+            Hand = 3,
+            /// <summary>
+            /// メッセージ（問い合わせ）
+            /// </summary>
+            Question = 4,
+        }
+        public enum ButtonType : int
+        {
+            OK = 0,
+            YesNo = 1,
+        }
+        public enum DialogResult : int
+        {
+            OK = 0,
+            Yes = 1,
+            No = 2,
+        }
+
+        public static DialogResult dr = 0;
+        public static DialogResult Show(Window _w, string _text, string _title, MessageType _mType, ButtonType _bType = ButtonType.OK) {
+            if (_bType == ButtonType.OK)
+            {
+                ExOKMassageBox okMsgBox = new ExOKMassageBox(_w, _text, _title, _mType);
+
+                _w.IsEnabled = false;
+                okMsgBox.ShowDialog();
+                _w.IsEnabled = true;
+            }
+
+            if (_bType == ButtonType.YesNo)
+            {
+                ExYesNoMessageBox yesnoMsgBox = new ExYesNoMessageBox(_w, _text, _title, _mType);
+
+                _w.IsEnabled = false;
+                yesnoMsgBox.ShowDialog();
+                _w.IsEnabled = true;
+            }
+
+            return dr;
+        }
+    }
+}
---
+++ SyousetsukaGetter/ExMessageBox/ExYesNoMessageBox.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace SyousetsukaGetter.ExMessageBox
+{
+    /// <summary>
+    /// ExYesNoMessageBox.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ExYesNoMessageBox : Window
+    {
+        public class ViewModel : System.ComponentModel.INotifyPropertyChanged
+        {
+            public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+            protected void OnPropertyChanged(string propertyName)
+            {
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+                }
+            }
+
+            string _MsgText = string.Empty;
+            public string MsgText
+            {
+                set
+                {
+                    _MsgText = value;
+                    OnPropertyChanged("MsgText");
+                }
+                get
+                {
+                    return _MsgText;
+                }
+            }
+            string _MSGTitle = string.Empty;
+            public string MSGTitle
+            {
+                set
+                {
+                    _MSGTitle = value;
+                    OnPropertyChanged("MSGTitle");
+                }
+                get
+                {
+                    return _MSGTitle;
+                }
+            }
+        }
+
+
+        ViewModel vm;
+        ExWindow.wInOut wIO;
+
+        Window w;
+        string text = string.Empty;
+        string title = string.Empty;
+        ExMessageBoxBase.MessageType mType;
+        public ExYesNoMessageBox(Window _w, string _text, string _title, ExMessageBoxBase.MessageType _mType)
+        {
+            InitializeComponent();
+
+            vm = new ViewModel();
+            DataContext = vm;
+
+            wIO = new ExWindow.wInOut(this, AroundBorder);
+
+            w = _w;
+            text = _text;
+            title = _title;
+            mType = _mType;
+
+            this.Owner = _w;
+        }
+
+        private void MainWindowCloseBT_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            HwndSource source = (HwndSource)HwndSource.FromVisual(this);
+            ExMessageBoxBase.FlashWind(source.Handle);
+
+            vm.MsgText = text;
+            vm.MSGTitle = title;
+
+            if (mType == ExMessageBoxBase.MessageType.Asterisk)
+            {
+                System.Media.SystemSounds.Asterisk.Play();
+            }
+            if (mType == ExMessageBoxBase.MessageType.Beep)
+            {
+                System.Media.SystemSounds.Beep.Play();
+            }
+            if (mType == ExMessageBoxBase.MessageType.Exclamation)
+            {
+                System.Media.SystemSounds.Exclamation.Play();
+            }
+            if (mType == ExMessageBoxBase.MessageType.Hand)
+            {
+                System.Media.SystemSounds.Hand.Play();
+            }
+            if (mType == ExMessageBoxBase.MessageType.Question)
+            {
+                System.Media.SystemSounds.Question.Play();
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            
+        }
+
+        private void NoBT_Click(object sender, RoutedEventArgs e)
+        {
+            ExMessageBoxBase.dr = ExMessageBoxBase.DialogResult.No;
+            Close();
+        }
+        private void YesBT_Click(object sender, RoutedEventArgs e)
+        {
+            ExMessageBoxBase.dr = ExMessageBoxBase.DialogResult.Yes;
+            Close();
+        }
+    }
+}
